3dfb4a15fb0157fc7165ede6687d69af4b4715ca
andrealtmann <andre@altmann.eu>
2020-06-11 02:47:49 +0100

    initial commit, adapted from FTD project

---
+++ R/run_spatial_statistics.R
@@ -0,0 +1,34 @@
+#! /usr/local/bin/Rscript
+#This is a wrapper script for spatial_statistics.R
+
+possible_samples <- c("H0351_1009", "H0351_1012", "H0351_1015", "H0351_1016", "H0351_2001", "H0351_2002")
+possible_ftd_genes <- c("FA","MD")
+
+remotely <- T
+sid <- ""
+
+#read comman line
+args = commandArgs(trailingOnly=TRUE)
+for (ar in args){
+  tok = strsplit(ar, split="=")[[1]]
+
+  if (tok[1] == "--feat"){
+    if (is.element(tok[2], possible_ftd_genes)){
+      ftd_gene <- tok[2]
+    } else {
+       message("feature ", tok[2], " is not a valid option.")
+       q("no",status=-1)
+    }
+  }
+
+  if (tok[1] == "--subj"){
+    if (is.element(tok[2], possible_samples)){
+      sid <- tok[2]
+    } else {
+       message("subjectID '", tok[2], "' is not a valid option.")
+       q("no",status=-1)
+    }
+  }
+}
+
+source("./spatial_statistics.R")
